6694ee68266ab94a4161ff724226153691122a54
nicomiguelino <nicomiguelino2014@gmail.com>
2023-07-19 14:43:33 -0700

    Add another `setInterval` example.

--- timers/index.js
+++ timers/index.js
@@ -9,4 +9,11 @@ const doSomething2 = () => {
 console.log('firing up timer/s')
 // NOTE: setInterval appliesa delay first before executing the function
 // setInterval(doSomething1, 3000);
-setTimeout(doSomething2, 5000);
+// setTimeout(doSomething2, 5000);
+
+
+// Another setInterval example.
+setInterval(function hello() {
+  console.log('Another setInterval example...')
+  return hello
+}(), 3000)
