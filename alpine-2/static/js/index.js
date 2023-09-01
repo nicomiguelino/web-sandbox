10ae06e4e0880fdadcf9a68049824cba7dab8650
nicomiguelino <nicomiguelino2014@gmail.com>
2023-09-01 15:10:23 -0700

    Write a simple counter application with Alpine.js.

--- alpine-2/static/js/index.js
+++ alpine-2/static/js/index.js
@@ -1,3 +1,17 @@
+const getMessage = () => {
+  return {
+    message: "I love Alpine!"
+  };
+};
+
+const getCount = (n = 1) => {
+  return {
+    count: 0,
+    increment() {
+      this.count += n;
+    }
+  };
+};
+
 window.onload = function() {
-  console.log("Hello World!");
 };
