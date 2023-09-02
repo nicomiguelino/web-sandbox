c2ea49db2df4f52f09c8500e97e3f412493ba176
nicomiguelino <nicomiguelino2014@gmail.com>
2023-09-01 19:50:45 -0700

    Make use of the `Alpine.store` function.

--- alpine-2/static/js/index.js
+++ alpine-2/static/js/index.js
@@ -1,55 +1,40 @@
-const getMessage = () => {
-  return {
-    message: "I love Alpine!"
-  };
-};
+document.addEventListener("alpine:init", () => {
+  Alpine.data("getMessage", () => {
+    return {
+      message: "I love Alpine more than ever!"
+    };
+  });
 
-const getCount = (n = 1) => {
-  return {
-    count: 0,
-    increment() {
-      this.count += n;
-    }
-  };
-};
+  Alpine.data("getSearchData", () => {
+    return {
+      search: "",
+      items: [
+        "Turtwig",
+        "Grotle",
+        "Torterra",
+        "Chimchar",
+        "Monferno",
+        "Infernape",
+        "Piplup",
+        "Prinplup",
+        "Empoleon",
+        "Starly",
+        "Staravia",
+        "Staraptor",
+      ],
+      get filteredItems() {
+        const fuse = new Fuse(this.items);
+        return fuse.search(this.search).map(({ item }) => item);
+      }
+    };
+  });
 
-const getSearchData = () => {
-  return {
-    search: "",
-    items: [
-      "Turtwig",
-      "Grotle",
-      "Torterra",
-      "Chimchar",
-      "Monferno",
-      "Infernape",
-      "Piplup",
-      "Prinplup",
-      "Empoleon",
-      "Starly",
-      "Staravia",
-      "Staraptor",
-    ],
-    get filteredItems() {
-      const fuse = new Fuse(this.items);
-      return fuse.search(this.search).map(({ item }) => item);
-    }
-  };
-};
-
-window.onload = () => {
-  const store = {
-    _count: 0,
-    get count() {
-      return this._count;
-    },
-    set count(x) {
-      this._count = x;
-    },
-  };
-
-  console.log(store.count);
-
-  store.count = 12;
-  console.log(store.count);
-};
+  Alpine.data("getCount", (n = 1) => {
+    return {
+      count: 0,
+      increment() {
+        this.count += n;
+      }
+    };
+  });
+});
